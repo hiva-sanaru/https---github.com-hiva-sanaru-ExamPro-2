a28b14f34c93e539fc571537f756c1e4de0dbd86
佐鳴予備校広報室 <sanaru123456@gmail.com>
2025-08-23 11:24:15 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "exampro-2-z3fpi",
-  appId: "1:552284590150:web:669ae96b51df245488ba47",
-  storageBucket: "exampro-2-z3fpi.firebasestorage.app",
-  apiKey: "AIzaSyCf-k-Keo54pBqvHjiUO796kT7tGP8WuHI",
-  authDomain: "exampro-2-z3fpi.firebaseapp.com",
-  messagingSenderId: "552284590150",
+  "projectId": "exampro-2-z3fpi",
+  "appId": "1:552284590150:web:669ae96b51df245488ba47",
+  "storageBucket": "exampro-2-z3fpi.firebasestorage.app",
+  "apiKey": "AIzaSyCf-k-Keo54pBqvHjiUO796kT7tGP8WuHI",
+  "authDomain": "exampro-2-z3fpi.firebaseapp.com",
+  "messagingSenderId": "552284590150"
 };
 
 // Initialize Firebase
