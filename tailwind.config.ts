41e948befd2067498b78b0f31ba53b4afa80d62d
佐鳴予備校広報室 <sanaru123456@gmail.com>
2025-08-24 11:50:50 +0000

    タイマーのフォントを変えてみて

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['Space Grotesk', 'sans-serif'],
-        code: ['monospace'],
+        code: ['Space Grotesk', 'monospace'],
       },
       colors: {
         background: 'hsl(var(--background))',
